51cb8a95a74373dd4ef3aa93d85dd6eef6b7f0e7
Orianeds <oriane.dasilva@yahoo.fr>
2023-10-04 14:49:24 +0200

    finalisation du niveau 1 et creation d'une branche

--- resoc_n1/tags.php
+++ resoc_n1/tags.php
@@ -52,7 +52,7 @@
                 <section>
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez les derniers messages comportant
-                        le mot-clé XXX
+                        le mot-clé <?php echo $tag['label']; ?>
                         (n° <?php echo $tagId ?>)
                     </p>
 
@@ -62,12 +62,13 @@
                 <?php
                 /**
                  * Etape 3: récupérer tous les messages avec un mot clé donné
+                 * Dans la requête ci-dessous, le COUNT (ligne 73) était en minuscule initialement
                  */
                 $laQuestionEnSql = "
                     SELECT posts.content,
                     posts.created,
                     users.alias as author_name,  
-                    count(likes.id) as like_number,  
+                    COUNT(likes.id) as like_number,  
                     GROUP_CONCAT(DISTINCT tags.label) AS taglist 
                     FROM posts_tags as filter 
                     JOIN posts ON posts.id=filter.post_id
@@ -91,29 +92,22 @@
                 while ($post = $lesInformations->fetch_assoc())
                 {
 
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
+                    // echo "<pre>" . print_r($post, 1) . "</pre>";
                     ?>                
                     <article>
                         <h3>
-                            <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
+                            <time datetime='2020-02-01 11:12:13' ><?php echo $post['created']; ?></time>
                         </h3>
-                        <address>par AreTirer</address>
+                        <address>par <?php echo $post['author_name']; ?></address>
                         <div>
-                            <p>Ceci est un paragraphe</p>
-                            <p>Ceci est un autre paragraphe</p>
-                            <p>... de toutes manières il faut supprimer cet 
-                                article et le remplacer par des informations en 
-                                provenance de la base de donnée</p>
+                            <p><?php echo $post['content']; ?></p>
                         </div>                                            
                         <footer>
-                            <small>♥ 132</small>
-                            <a href="">#lorem</a>,
-                            <a href="">#piscitur</a>,
+                            <small>♥ <?php echo $post['like_number']; ?></small>
+                            <a href="">#<?php echo $post['taglist']; ?></a>
                         </footer>
                     </article>
                 <?php } ?>
-
-
             </main>
         </div>
     </body>
