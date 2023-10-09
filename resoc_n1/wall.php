caa309b4fe8170e55678be57420a4e9bfd0a9337
FranceHuon <133089058+FranceHuon@users.noreply.github.com>
2023-10-09 16:48:02 +0200

    essais boutons s'abonner/se désabonner

--- resoc_n1/wall.php
+++ resoc_n1/wall.php
@@ -59,6 +59,7 @@ session_start();
                // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
                 //     // si on recoit un champs email rempli il y a une chance que ce soit un traitement
                 $verification = isset($_POST['abonnement']);
+                // $check = isset($_POST['désabonnement']);
                 if($verification) {
                   // on ne fait ce qui suit que si un formulaire a été soumis.
                   // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
@@ -75,7 +76,13 @@ session_start();
                   $lInstructionSql = "INSERT INTO followers (id, followed_user_id, following_user_id) "
                                 . "VALUES (NULL, " . $followerId . ", " . "'" . $userId . "');"
                                 ;
-                        // echo $lInstructionSql;
+                // echo $lInstructionSql;
+
+                $SQLFollowers = "SELECT * FROM followers WHERE `followed_user_id`= $followerId AND `following_user_id`= $userId";
+                $Appel = $mysqli->query($SQLFollowers);
+
+                $SQLDelete = "DELETE FROM `followers` WHERE `followed_user_id`= $followerId AND `following_user_id`= $userId";
+                $AppelDelete = $mysqli->query($SQLDelete);
 
                   // Etape 5 : execution
                   $ok = $mysqli->query($lInstructionSql);
@@ -86,16 +93,31 @@ session_start();
                   } else
                   {
                       echo "Vous êtes bien abonné à " . $user['alias'];
+                      echo $_SESSION['connected_id'];
+                      echo $userId;
                   }
                 }
-
-                if (!("wall.php?user_id=" . $user['id'] == "wall.php?user_id=" . $_SESSION['connected_id'])) {
-                ?>  <form action="wall.php?user_id=<?php echo $user['id']; ?>" method="post">
-                    <input type="submit" name="abonnement" class="button" value="S'abonner">
-                    <!-- <input type="submit" name="désabonnement" class="button" value="Se désabonner" > -->
-                </form>
+                if (!("wall.php?user_id=" . $userId == "wall.php?user_id=" . $_SESSION['connected_id'])) {
+                ?> 
                 <?php 
-                    // if("wall.php?user_id=" . $user['id'] == "wall.php?user_id=")
+                    if (($Appel)) {
+                        
+
+                ?>
+                        <form action="wall.php?user_id=<?php echo $user['id']; ?>" method="post">
+                        <input type="submit" name="désabonnement" class="button" value="Se désabonner" >
+                        </form>
+                        
+                    
+                <?php
+                echo "<pre>"  . print_r($AppelDelete,1) . "</pre>";
+                    } else {
+                ?>
+                        <form action="wall.php?user_id=<?php echo $user['id']; ?>" method="post">
+                        <input type="submit" name="abonnement" class="button" value="S'abonner">
+                        </form>
+                <?php
+                    }
                 }
                 ?>
                     <p>Sur cette page vous trouverez tous les message de l'utilisatrice : <?php echo $user['alias']; ?>
